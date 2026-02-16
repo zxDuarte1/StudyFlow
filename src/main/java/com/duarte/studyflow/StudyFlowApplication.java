ff9400003e1476082566b0d63c299b58a70f3bb3
Your Name <xbielx192@gmail.com>
2026-02-16 12:46:59 -0300

    first commit

---
+++ src/main/java/com/duarte/studyflow/StudyFlowApplication.java
@@ -0,0 +1,13 @@
+package com.duarte.studyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duarte/studyflow/StudyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duarte.studyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
